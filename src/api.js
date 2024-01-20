9f0becca38bfe13d0d796d53c6fd70e490c8cd6a
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2024-01-19 23:24:05 -0800

    null

--- src/api.js
+++ src/api.js
@@ -10,4 +10,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 
-export const WEATHER_API_KEY = "YOUR OWN WEATHER API KEY FROM OPEN WEATHER";
+export const WEATHER_API_KEY = "YOUR OWN WEATHER API KEY FROM OPEN WEATHER"; 
